c01741a032b93a5fc4d0bc7878a4b2248373891f
Mohamed Adel Elsayed Yamany <Mohamedadelyamany921@gmail.com>
2022-11-17 12:56:29 +0200

    first commit to upload files

--- src/App.js
+++ src/App.js
@@ -1,38 +1,109 @@
-import { useState } from 'react';
+import { useState ,useEffect , useRef} from 'react';
 import './App.css';
 import FlashcardList from './FlashcardList';
+import axios from 'axios';
 
 
 function App() {
-  const [flashcards , setFlashcard] = useState(Sample_Flashcards)
+  const [flashcards , setFlashcards] = useState([])
+  const [categories , setCategories] = useState([])
+
+  const categoryEl = useRef();
+  const amountEl = useRef()
+
+  useEffect(() => {
+    axios
+    .get('https://opentdb.com/api_category.php')
+    .then(res => {
+      setCategories(res.data.trivia_categories)
+    })
+  }, [])
+
+  /*
+  used for testing the api
+  useEffect(()=>{  
+  },[]) */
+
+  // it is used to remove the weird html codes and convert them into string characters 
+  function decodeString(str){
+    let textArea = document.createElement("textarea")
+    textArea.innerHTML = str
+    return textArea.value;
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    axios.get('https://opentdb.com/api.php?amount',{
+      params :{
+        amount :amountEl.current.value,
+        category : categoryEl.current.value
+      }
+    }).then(res =>{
+      setFlashcards(res.data.results.map((questionItem , index)=>{
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [...questionItem.incorrect_answers.map(a => decodeString(a)),answer]
+        return {
+          id : `${index}-${Date.now()}`,
+          question : decodeString(questionItem.question),
+          answer : answer,
+          options : options.sort(()=> Math.random() - 0.5)
+        }
+      }))
+    })
+  }
+
   return (
-    <FlashcardList flashcards={flashcards} />
+    <>
+    <form className='header' onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <label htmlFor='category'>Category</label>
+        <select id='category' ref={categoryEl}>
+          {categories.map(category => {
+            return <option value={category.id} key={category.id}>
+              {category.name}
+            </option>
+          })}
+        </select>
+      </div>
+      <div className='form-group'>
+        <label htmlFor='amount'>Number of Questions</label>
+        <input type={'number'} id='amount' min='1' step='1' defaultValue={10} ref={amountEl}/>
+      </div>
+      <div className='form-group'>
+        <button className='btn'>Generate</button>
+      </div>
+    </form>
+    <div className='container'>
+      <FlashcardList flashcards={flashcards} />
+    </div>
+    </>
   );
 }
 
-const Sample_Flashcards = [
-  {
-    id :1,
-    question : 'Question1',
-    answer : 'Answer',
-    options : [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
-    ]
-  },
-  {
-    id : 2 ,
-    question : 'Question2',
-    answer : 'Answer',
-    options : [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
-    ]
-  }
-]
+// placeholder 
+// const Sample_Flashcards = [
+//   {
+//     id :1,
+//     question : 'Question1',
+//     answer : 'Answer',
+//     options : [
+//       "Answer 1",
+//       "Answer 2",
+//       "Answer 3",
+//       "Answer 4"
+//     ]
+//   },
+//   {
+//     id : 2 ,
+//     question : 'Question2',
+//     answer : 'Answer',
+//     options : [
+//       "Answer 1",
+//       "Answer 2",
+//       "Answer 3",
+//       "Answer 4"
+//     ]
+//   }
+// ]
 
 export default App;
--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,25 +1,45 @@
-import React ,{useState} from 'react'
+import React ,{useState , useEffect , useRef} from 'react'
 
 export default function Flashcard({flashcard}) {
     const [flip , setFlip] = useState(false)
+    const [height , setHeight] = useState('initial')
+
+    const frontEl = useRef()
+    const backEl = useRef()
+
+    function setMaxHeight (){
+        const frontHeight = frontEl.current.getBoundingClientRect().height;
+        const backHeight = backEl.current.getBoundingClientRect().height;
+        setHeight(Math.max(frontHeight,backHeight,100))
+    }
+
+    useEffect(setMaxHeight,[flashcard.question , flashcard.answer, flashcard.options])
+
+    useEffect(() => {
+        window.addEventListener("resize",setMaxHeight)
+        // this return is called whenever our component destroys itself  
+        return window.removeEventListener("resize",setMaxHeight)
+    },[])
+
     return (
         <div 
         className={`card ${flip ? 'flip' : ''}`}
+        style={{height : height}}
         // flip effect
         onClick={() => {setFlip(!flip)}} 
         >
-            <div className='front'>
+            <div className='front' ref={frontEl}>
                 {flashcard.question}
                 <div className='flashcard-options'>
                     {flashcard.options.map(option =>{
-                        return <div className='flashcard-option'>{option}</div>
+                        return <div className='flashcard-option' key={option}>{option}</div>
                     })}
                 </div>
             </div>
-            <div className='back'>
+            <div className='back' ref={backEl}>
                 {flashcard.answer}
             </div>
-            {flip ? flashcard.answer : flashcard.question}
+            {/* {flip ? flashcard.answer : flashcard.question} */}
         </div>
     )
 }
